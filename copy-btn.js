2195b5c6f524d976a1a545a772f3a9e40b4cd0d2
swehera <87908048+swehera@users.noreply.github.com>
2021-07-24 22:36:24 +0600

    wpm

---
+++ copy-btn.js
@@ -0,0 +1,59 @@
+// 01
+let codE01 = document.getElementById("codE01");
+    let copY01 = document.getElementById("copY01");
+
+    copY01.onclick = function () {
+      codE01.select();
+      document.execCommand("copy");
+      copY01.innerText = ("Success");
+copY01.style.backgroundColor = "#28db00";;
+copY01.style.color = "white";;
+    }
+
+// 001
+let codE001 = document.getElementById("codE001");
+    let copY001 = document.getElementById("copY001");
+
+    copY001.onclick = function () {
+      codE001.select();
+      document.execCommand("copy");
+      copY001.innerText = ("Success");
+copY001.style.backgroundColor = "#28db00";;
+copY001.style.color = "white";;
+    }
+
+// 31
+let codE31 = document.getElementById("codE31");
+    let copY31 = document.getElementById("copY31");
+
+    copY31.onclick = function () {
+      codE31.select();
+      document.execCommand("copy");
+      copY31.innerText = ("Success");
+copY31.style.backgroundColor = "#28db00";;
+copY31.style.color = "white";;
+    }
+
+// 41
+let codE41 = document.getElementById("codE41");
+    let copY41 = document.getElementById("copY41");
+
+    copY41.onclick = function () {
+      codE41.select();
+      document.execCommand("copy");
+      copY41.innerText = ("Success");
+copY41.style.backgroundColor = "#28db00";;
+copY41.style.color = "white";;
+    }
+
+// 51
+let codE51 = document.getElementById("codE51");
+    let copY51 = document.getElementById("copY51");
+
+    copY51.onclick = function () {
+      codE51.select();
+      document.execCommand("copy");
+      copY51.innerText = ("Success");
+copY51.style.backgroundColor = "#28db00";;
+copY51.style.color = "white";;
+    }
